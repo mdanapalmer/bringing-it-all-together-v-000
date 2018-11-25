1d04af0d5a303d9651abe1d62a231acee959c686
Dana Palmer <roiai4@hotmail.com>
2018-11-25 04:39:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,11 +61,11 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog.self.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
-    new_dog
+    self.new
+    self.id = row[0]
+    self.name = row[1]
+    self.breed = row[2]
+    self
   end
 
 end
