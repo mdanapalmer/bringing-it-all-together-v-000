b753233e11291caac240773114452b59afefb0f8
Dana Palmer <roiai4@hotmail.com>
2018-11-25 03:47:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,11 @@
-class Dog 
+class Dog
 
   attr_accessor :id, :name, :breed
 
-  def initialize (id: = nil, name:, :breed )
+  def initialize (id: = nil, name:, :breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+end
